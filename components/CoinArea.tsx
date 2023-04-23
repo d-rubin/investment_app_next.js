dfd78591a4ab0c826d3c32757fbb93bb5ea73b56
Daniel <contact@daniel-rubin.de>
2023-04-23 15:07:02 +0200

    fix: removed fetch useData

--- components/CoinArea.tsx
+++ components/CoinArea.tsx
@@ -1,18 +1,8 @@
-"use client";
-
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { DisplayCoin } from "./DisplayCoin";
 
 const CoinArea = () => {
-  const [ids, setIds] = useState([]);
-  const getUserData = async (user: string) => {
-    const response = await fetch("userData");
-    const json = await response.json();
-    setIds(json[user].ids);
-  };
-  useEffect(() => {
-    getUserData("tobi");
-  }, []);
+  const ids = ["bitcoin", "solana", "ethereum", "tether"];
 
   return (
     <div className="grid xs:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 px-4 gap-4 mb-4">
