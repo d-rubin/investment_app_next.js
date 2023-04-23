18aae7e260a4376955bef7241b735f2071704391
Daniel Rubin <daniel.rubin1997@gmail.com>
2023-04-23 21:40:36 +0200

    chore: added ids

--- components/CoinArea.tsx
+++ components/CoinArea.tsx
@@ -2,7 +2,16 @@ import React from "react";
 import { DisplayCoin } from "./DisplayCoin";
 
 const CoinArea = () => {
-  const ids = ["bitcoin", "solana", "ethereum", "tether"];
+  const ids = [
+    "bitcoin",
+    "ethereum",
+    "solana",
+    "binancecoin",
+    "arbitrum",
+    "polygon",
+    "dogecoin",
+    "cardano",
+  ];
 
   return (
     <div className="grid xs:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 px-4 gap-4 mb-4">
