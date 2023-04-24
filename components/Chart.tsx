07644003789b50df8a157be16dbbb94700b0718d
Daniel <contact@daniel-rubin.de>
2023-04-24 20:48:30 +0200

    fix: chart is working again

--- components/Chart.tsx
+++ components/Chart.tsx
@@ -1,4 +1,6 @@
-import React from "react";
+"use client";
+
+import React, { useEffect, useState } from "react";
 import { Line } from "react-chartjs-2";
 import {
   Chart as ChartJS,
@@ -11,8 +13,10 @@ import {
   Legend,
   ChartOptions,
   LineController,
+  ChartDataset,
   ChartData,
 } from "chart.js";
+import { Search } from "./Search";
 
 ChartJS.register(
   CategoryScale,
@@ -25,14 +29,27 @@ ChartJS.register(
   LineController
 );
 
-function CoinChart(props: {
-  data: ChartData<"line">;
-  // eslint-disable-next-line react/require-default-props
-  xTicksLimit?: number;
-  // eslint-disable-next-line react/require-default-props
-  yTicksLimit?: number;
-}) {
-  const { data, xTicksLimit, yTicksLimit } = props;
+interface ChartResponse {
+  prices: [[number, number]];
+  market_caps: [[number, number]];
+  total_volumes: [[number, number]];
+}
+
+function CoinChart({ id }: { id: string }) {
+  const [ids, setIds] = useState<string[]>([id]);
+  const [days, setDays] = useState<number>(7);
+  const [data, setData] = useState<ChartData<"line">>();
+  const colors: string[] = [
+    "blue",
+    "green",
+    "red",
+    "yellow",
+    "purple",
+    "black",
+    "lightgrey",
+  ];
+  let labels: string[] = [];
+  let chartDataSets: ChartDataset<"line">[] = [];
   const options: ChartOptions<"line"> = {
     responsive: true,
     maintainAspectRatio: false,
@@ -63,20 +80,126 @@ function CoinChart(props: {
           autoSkip: true,
           maxRotation: 0,
           minRotation: 0,
-          maxTicksLimit: xTicksLimit || undefined,
+          maxTicksLimit: 8,
         },
       },
       y: {
         position: "right",
         ticks: {
-          maxTicksLimit: yTicksLimit || undefined,
           autoSkip: true,
           source: "auto",
         },
       },
     },
   };
-  return <Line className="h-full w-full" data={data} options={options} />;
+
+  const fetchChartData = (nr: number) => {
+    chartDataSets = [];
+    ids.forEach(async (item, index) => {
+      const color = colors[index];
+      const pointData: ChartResponse = await fetch(
+        `https://api.coingecko.com/api/v3/coins/${item}/market_chart?vs_currency=eur&days=${nr}`
+      ).then((res) => res.json());
+      const prices: number[] = [];
+      labels = [];
+      for (let i = 0; i < pointData.prices.length; i += 1) {
+        prices.push(pointData.prices[i][1]);
+        labels.push(new Date(pointData.prices[i][0]).toLocaleDateString());
+      }
+      chartDataSets.push({
+        data: prices,
+        label: item,
+        fill: false,
+        pointBackgroundColor: color,
+        borderColor: color,
+        pointHitRadius: 3,
+        pointRadius: 0,
+        hoverBorderWidth: 5,
+      });
+      setData({ labels, datasets: chartDataSets });
+    });
+  };
+
+  const handleClick = (number: number) => {
+    setDays(number);
+  };
+
+  const handleSearchClick = (coin: string) => {
+    setIds([...ids, coin]);
+  };
+
+  useEffect(() => {
+    fetchChartData(days);
+  }, [ids, days]);
+
+  return (
+    <div className="flex flex-col items-center gap-2">
+      <Search onClick={(coin) => handleSearchClick(coin)} />
+      <div className="flex flex-wrap justify-center w-full gap-2">
+        <button
+          onClick={() => handleClick(1825)}
+          className="row bg-blue-400 text-white active:bg-blue-600 font-bold uppercase text-xs px-4 py-2
+                      rounded-lg shadow hover:shadow-md outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+          type="button"
+        >
+          5 Jahre
+        </button>
+        <button
+          onClick={() => handleClick(1095)}
+          className="row bg-blue-400 text-white active:bg-blue-600 font-bold uppercase text-xs px-4 py-2
+                      rounded-lg shadow hover:shadow-md outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+          type="button"
+        >
+          3 Jahre
+        </button>
+        <button
+          onClick={() => handleClick(365)}
+          className="row bg-blue-400 text-white active:bg-blue-600 font-bold uppercase text-xs px-4 py-2
+                      rounded-lg shadow hover:shadow-md outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+          type="button"
+        >
+          1 Jahr
+        </button>
+        <button
+          onClick={() => handleClick(90)}
+          className="row bg-blue-400 text-white active:bg-blue-600 font-bold uppercase text-xs px-4 py-2
+                      rounded-lg shadow hover:shadow-md outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+          type="button"
+        >
+          3 Monate
+        </button>
+        <button
+          onClick={() => handleClick(30)}
+          className="row bg-blue-400 text-white active:bg-blue-600 font-bold uppercase text-xs px-4 py-2
+                      rounded-lg shadow hover:shadow-md outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+          type="button"
+        >
+          1 Monat
+        </button>
+        <button
+          onClick={() => handleClick(7)}
+          className="row bg-blue-400 text-white active:bg-blue-600 font-bold uppercase text-xs px-4 py-2
+                      rounded-lg shadow hover:shadow-md outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+          type="button"
+        >
+          7 Tage
+        </button>
+        <button
+          onClick={() => handleClick(1)}
+          className="row bg-blue-400 text-white active:bg-blue-600 font-bold uppercase text-xs px-4 py-2
+                      rounded-lg shadow hover:shadow-md outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+          type="button"
+        >
+          1 Tag
+        </button>
+      </div>
+      <div className="h-80 w-full">
+        {data && (
+          <Line className="h-full w-full" data={data} options={options} />
+        )}
+      </div>
+    </div>
+  );
 }
 
 export { CoinChart };
