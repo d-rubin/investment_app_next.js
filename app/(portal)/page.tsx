4174b09fdf462875634e8ff1e2c0d40159471471
Daniel <contact@daniel-rubin.de>
2023-07-14 17:43:51 +0200

    feat: worked on Overview

--- app/(portal)/page.tsx
+++ app/(portal)/page.tsx
@@ -1,3 +1,22 @@
+import React from "react";
+
 export default function RootPage() {
-  return <h1>Hallo</h1>;
+  return (
+    <main className="w-full h-full pt-20">
+      <h1 className="w-full h-20 pl-4 pt-4 text-4xl font-semibold fixed bg-black left-0 top-0 shadow-xl">
+        Overview
+      </h1>
+      <section className="grid grid-cols-2 gap-4 w-full h-full items-center justify-center">
+        {Array.from({ length: 8 }).map(() => {
+          return (
+            <article className="w-full h-full bg-gray-500 rounded-2xl">
+              <span>
+                <p>ETH</p>
+              </span>
+            </article>
+          );
+        })}
+      </section>
+    </main>
+  );
 }
