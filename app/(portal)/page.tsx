f5de4bb9bb85fa2f7ff157bc510667d0605f90bc
Daniel <contact@daniel-rubin.de>
2023-07-19 18:02:34 +0200

    refactor: changed data to 7d instead of 24h

--- app/(portal)/page.tsx
+++ app/(portal)/page.tsx
@@ -1,16 +1,19 @@
 import React from "react";
-import CoinList from "../../components/CoinList";
+import Coin from "../../components/Coin";
+import { getCoinList } from "../../helper";
 
-export default function RootPage() {
+export default async function RootPage() {
+  const coinList = await getCoinList();
+
   return (
     <main className="w-full h-full pt-24">
       <h1 className="w-full max-w-screen-xl mx-auto h-20 pl-4 pt-4 text-4xl font-semibold fixed bg-black top-0 shadow-xl">
         Overview
       </h1>
       <section className="grid grid-cols-2 sm:grid-cols-4 md:grid-cols-6 gap-4 w-full h-fit justify-items-center">
-        {/* eslint-disable-next-line @typescript-eslint/ban-ts-comment */}
-        {/* @ts-ignore */}
-        <CoinList />
+        {coinList.map((coin) => {
+          return <Coin coin={coin} />;
+        })}
       </section>
     </main>
   );
