466ca1e065e81242b11cc1bf39fc38e41b7ccf00
Daniel <contact@daniel-rubin.de>
2023-07-23 16:11:56 +0200

    feat: added vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "../styles/globals.css";
+import { Analytics } from "@vercel/analytics/react";
 
 export const metadata = {
   title: "Crpyto-meeting",
@@ -10,7 +11,10 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <body className="scrollbar-none">
-        <div className="px-4 max-w-screen-lg m-auto w-full h-full">{children}</div>
+        <div className="px-4 max-w-screen-lg m-auto w-full h-full">
+          {children}
+          <Analytics />
+        </div>
       </body>
     </html>
   );
