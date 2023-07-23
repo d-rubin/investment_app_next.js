9deaebfff7f21a95bab6112d70c73c2f5f814dd3
Daniel <contact@daniel-rubin.de>
2023-07-23 16:19:09 +0200

    feat: rebased and try to fix background

--- app/(portal)/page.tsx
+++ app/(portal)/page.tsx
@@ -7,7 +7,7 @@ export default async function RootPage() {
 
   return (
     <main className="relative w-full h-full pt-24 px-4 max-w-screen-lg mx-auto">
-      <div className="w-full flex flex-row justify-between items-center py-4 shadow-xl z-10 bg-black fixed top-0 max-w-screen-lg mx-auto">
+      <div className="w-full py-4 shadow-xl z-10 bg-black fixed top-0 max-w-screen-lg mx-auto">
         <h1 className="text-4xl font-semibold ">Overview</h1>
       </div>
       <section className="grid grid-cols-2 sm:grid-cols-4 md:grid-cols-6 gap-4 w-full h-fit justify-items-center">
