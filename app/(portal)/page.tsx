23fcdd5bdda836a4febc61433274dfa7c578aa20
Daniel <contact@daniel-rubin.de>
2023-07-23 16:07:27 +0200

    chore: removed walletconnect

--- app/(portal)/page.tsx
+++ app/(portal)/page.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import WalletConnect from "@/components/WalletConnect";
 import { getCoinList } from "@/helper";
 import Coin from "../../components/Coin";
 
@@ -8,9 +7,8 @@ export default async function RootPage() {
 
   return (
     <main className="relative w-full h-full pt-24 px-4 max-w-screen-lg mx-auto">
-      <div className="flex flex-row justify-between items-center py-4 shadow-xl z-10 bg-black fixed pr-8 top-0 max-w-screen-lg mx-auto">
+      <div className="flex flex-row justify-between items-center py-4 shadow-xl z-10 bg-black fixed top-0 max-w-screen-lg mx-auto">
         <h1 className="text-4xl font-semibold ">Overview</h1>
-        <WalletConnect />
       </div>
       <section className="grid grid-cols-2 sm:grid-cols-4 md:grid-cols-6 gap-4 w-full h-fit justify-items-center">
         {coinList?.map((coin) => {
