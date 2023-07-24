6433fe9367e62218c2356320f44c7e6b62df3d34
Daniel <contact@daniel-rubin.de>
2023-07-24 20:34:33 +0200

    feat: added Walletconnect again

--- app/(portal)/page.tsx
+++ app/(portal)/page.tsx
@@ -1,19 +1,21 @@
 import React from "react";
 import { getCoinList } from "@/helper";
+import WalletConnect from "@/components/WalletConnect";
 import Coin from "../../components/Coin";
 
 export default async function RootPage() {
-  const coinList = await getCoinList();
+  // const coinList = await getCoinList();
 
   return (
     <main className="relative w-full h-full pt-24 px-4 max-w-screen-lg mx-auto">
       <div className="w-full py-4 shadow-xl z-10 bg-black fixed top-0 max-w-screen-lg mx-auto">
         <h1 className="text-4xl font-semibold ">Overview</h1>
       </div>
+      <WalletConnect />
       <section className="grid grid-cols-2 sm:grid-cols-4 md:grid-cols-6 gap-4 w-full h-fit justify-items-center">
-        {coinList?.map((coin) => {
-          return <Coin key={coin.id} coin={coin} />;
-        })}
+        {/* {coinList?.map((coin) => { */}
+        {/*  return <Coin key={coin.id} coin={coin} />; */}
+        {/* })} */}
       </section>
     </main>
   );
