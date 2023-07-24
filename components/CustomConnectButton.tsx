2792a2865841b0bfe5b6e089b7eedf5bd5b9d7f7
Daniel <contact@daniel-rubin.de>
2023-07-24 22:28:20 +0200

    feat worked on transactions

---
+++ components/CustomConnectButton.tsx
@@ -0,0 +1,106 @@
+"use client";
+
+import { SignClient } from "@walletconnect/sign-client";
+import { useEffect, useState } from "react";
+import { SignClient as SignClientType } from "@walletconnect/sign-client/dist/types/client";
+import { Web3Modal } from "@web3modal/standalone";
+import { SessionTypes } from "@walletconnect/types";
+
+const projectId = process.env.PROJECT_ID!;
+
+// @ts-ignore
+const web3modal = new Web3Modal({
+  projectId,
+  standaloneChains: ["eip155:1"],
+});
+
+const CustomConnectButton = () => {
+  const [signClient, setSignClient] = useState<SignClientType>();
+  const [sessions, setSessions] = useState<SessionTypes.Struct>();
+  const [accounts, setAccounts] = useState<string>();
+
+  const createClient = async () => {
+    try {
+      const client = await SignClient.init({
+        projectId,
+      });
+      // @ts-ignore
+      setSignClient(client);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const onSessionConnect = async (session: SessionTypes.Struct) => {
+    if (!session) throw new Error("client doesn't exist");
+    try {
+      setSessions(session);
+      setAccounts(session.namespaces.eip155.accounts[0].slice(9));
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  const handleConnect = async () => {
+    if (!signClient) throw new Error("Cannot connect. Sign Client is not created");
+    try {
+      const proposalNamespace = {
+        eip155: {
+          chains: ["eip155:1"],
+          methods: ["eth_sendTransaction"],
+          events: ["connect", "disconnect"],
+        },
+      };
+
+      const { uri, approval } = await signClient.connect({
+        requiredNamespaces: proposalNamespace,
+      });
+
+      if (uri) {
+        await web3modal.openModal({ uri });
+        const sessionNamespace = await approval();
+        onSessionConnect(sessionNamespace);
+        web3modal.closeModal();
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  // todo: dosnt't work like in video
+  const reset = () => {
+    setAccounts(undefined);
+    setSessions(undefined);
+  };
+
+  const handleDisconnect = async () => {
+    try {
+      await signClient?.disconnect({
+        topic: sessions!.topic,
+        reason: { message: "User disconnected", code: 6000 },
+      });
+      reset();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    if (!signClient) {
+      createClient();
+    }
+  }, [signClient]);
+
+  return accounts?.length ? (
+    <>
+      <p>{accounts}</p>
+      <button onClick={handleDisconnect}>Disconnect</button>
+    </>
+  ) : (
+    <button onClick={handleConnect} disabled={!signClient}>
+      Connect
+    </button>
+  );
+};
+
+export default CustomConnectButton;
