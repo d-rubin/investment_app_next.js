7b1b3f023a01471d03152335d09dae3512ac2f95
Daniel <contact@daniel-rubin.de>
2023-08-04 23:01:17 +0200

    feat: added revalidation to getCoinList()

--- helper/index.ts
+++ helper/index.ts
@@ -1,11 +1,12 @@
 import { CoinData } from "../interfaces";
 
-const getCoinList = (): Promise<CoinData[] | []> => {
+const getCoinList = async (): Promise<CoinData[] | []> => {
   return fetch(
-    "https://api.coingecko.com/api/v3/coins/markets?vs_currency=eur&order=market_cap_desc&per_page=100&page=1&sparkline=true&price_change_percentage=7d&locale=de&precision=2"
+    "https://api.coingecko.com/api/v3/coins/markets?vs_currency=eur&order=market_cap_desc&per_page=100&page=1&sparkline=true&price_change_percentage=7d&locale=de&precision=2",
+    { next: { revalidate: 60 } },
   ).then((res) => {
     if (res.status !== 200) {
-      console.log("ERROR", res);
+      console.log("ERROR", res.json());
       return [];
     }
     return res.json();
