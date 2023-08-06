0ed9b43ea3875831f20a4955ccde6358887ed2db
Daniel <contact@daniel-rubin.de>
2023-08-06 18:46:27 +0200

    feat: no-cache for getCoinList()

--- helper/index.ts
+++ helper/index.ts
@@ -1,9 +1,9 @@
-import { CoinData } from "../interfaces";
+import { CoinData } from "@/interfaces";
 
 const getCoinList = async (): Promise<CoinData[] | []> => {
   return fetch(
     "https://api.coingecko.com/api/v3/coins/markets?vs_currency=eur&order=market_cap_desc&per_page=100&page=1&sparkline=true&price_change_percentage=7d&locale=de&precision=2",
-    { next: { revalidate: 60 } },
+    { cache: "no-cache", next: { revalidate: 60 } },
   ).then((res) => {
     if (res.status !== 200) {
       console.log("ERROR", res.json());
