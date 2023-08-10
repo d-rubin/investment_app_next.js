694b6e591d33d434ef2125b8bf620d1461db9525
Daniel <contact@daniel-rubin.de>
2023-08-10 19:43:17 +0200

    refactor: removes suspense

--- app/(portal)/page.tsx
+++ app/(portal)/page.tsx
@@ -16,7 +16,20 @@ export default function RootPage() {
     });
   }, []);
 
-  const loadingItems = (
+  return coinList ? (
+    <main className="relative w-full h-full pt-24 px-4 max-w-screen-lg mx-auto">
+      <div className="w-full py-4 shadow-xl z-10 bg-black fixed top-0 max-w-screen-lg mx-auto">
+        <h1 className="text-4xl font-semibold ">Overview</h1>
+      </div>
+      <WalletConnect />
+      {/* <CustomConnectButton /> */}
+      <section className="grid grid-cols-2 sm:grid-cols-4 lg:grid-cols-6 gap-4 w-full h-fit justify-items-center">
+        {coinList?.map((coin) => {
+          return <Coin key={coin.id} coin={coin} />;
+        })}
+      </section>
+    </main>
+  ) : (
     <main className="w-full h-full pt-24">
       <h1 className="w-full max-w-screen-xl mx-auto h-20 pl-4 pt-4 text-4xl font-semibold fixed bg-black top-0 shadow-xl">
         Overview
@@ -32,22 +45,5 @@ export default function RootPage() {
         })}
       </section>
     </main>
-  );
-
-  return (
-    <Suspense fallback={loadingItems}>
-      <main className="relative w-full h-full pt-24 px-4 max-w-screen-lg mx-auto">
-        <div className="w-full py-4 shadow-xl z-10 bg-black fixed top-0 max-w-screen-lg mx-auto">
-          <h1 className="text-4xl font-semibold ">Overview</h1>
-        </div>
-        <WalletConnect />
-        {/* <CustomConnectButton /> */}
-        <section className="grid grid-cols-2 sm:grid-cols-4 lg:grid-cols-6 gap-4 w-full h-fit justify-items-center">
-          {coinList?.map((coin) => {
-            return <Coin key={coin.id} coin={coin} />;
-          })}
-        </section>
-      </main>
-    </Suspense>
   );
 }
